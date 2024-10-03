f26124f3ad929a114073cbdabd95783526c2b168
Samuel Bosawer <zam.bosawer@gmail.com>
2024-10-03 19:51:47 +0900

    Pewarisan

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ class Mobil {
         echo 'Warna :' . $this->warna.'<br>';
         echo 'Pemilik :' . $nama.'<br>';
         echo 'Keterangan :' . $ket.'<br>';
+        echo '<br>';
 
     }
 
@@ -33,8 +34,20 @@ class Mobil {
 
 }
 
+ class Kendaraan extends Mobil  {
+    public $jenis;
+
+    public function bergerak()
+    {
+        echo 'Kendaraan sedang bergerak';
+    }
+ }
+
+
+
 // Objek 
 $mobil = new Mobil();
+$kendaraan = new Kendaraan();
 $mobil->merk = 'Avanza';
 $mobil->model = 'Nova';
 $mobil->tahun = 2023;
@@ -44,6 +57,10 @@ $mobil->warna = 'Putih';
 $ket =' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ducimus alias ab quia sunt reprehenderit consequuntur possimus inventore tempore deserunt.';
 
 $mobil->cek('Samuel',$ket);
-
+$kendaraan->merk = 'BWM';
+$kendaraan->model = 'Nova';
+$kendaraan->tahun = 2023;
+$kendaraan->warna = 'Putih';
+$kendaraan->cek('Sam','');
 
 
